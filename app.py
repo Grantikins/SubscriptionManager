9a44ed2df9e88f9000accc929dfcca39da8f7916
Grantikins <grantikinsr@gmail.com>
2023-02-19 20:02:17 -0600

    heroku test

--- app.py
+++ app.py
@@ -18,4 +18,4 @@ db.init_app(app)
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=False, port=os.environ.get("PORT"))
+    app.run(debug=False, port=int(os.environ.get("PORT", 5000)))
